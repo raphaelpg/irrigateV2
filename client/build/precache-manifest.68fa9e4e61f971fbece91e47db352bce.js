19246eb145d6bacbc5bb388190834e1320a53795
who <raphael_pinto@outlook.com>
2020-11-25 12:36:46 +0100

    build client

---
+++ client/build/precache-manifest.68fa9e4e61f971fbece91e47db352bce.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0a1037bdfe6986482e923f745822a01",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f292897f117f732202dc",
+    "url": "/static/css/main.8b56c66d.chunk.css"
+  },
+  {
+    "revision": "f4407aa4ff295b7af709",
+    "url": "/static/js/2.3d36001c.chunk.js"
+  },
+  {
+    "revision": "259f224f3a0b820fabbf3ff1ff983ef6",
+    "url": "/static/js/2.3d36001c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f292897f117f732202dc",
+    "url": "/static/js/main.3a9fa87b.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "fe82d4bebb545cd0d8c9683e258e961c",
+    "url": "/static/media/Muli-Regular.fe82d4be.ttf"
+  },
+  {
+    "revision": "5fcf988f1d07a2b7ed1f926bb720ab49",
+    "url": "/static/media/america.5fcf988f.png"
+  },
+  {
+    "revision": "cf79342375a32a5d470e16c39b158443",
+    "url": "/static/media/asia.cf793423.png"
+  },
+  {
+    "revision": "3a2992621a3ab7b61b4ad591d5058f2a",
+    "url": "/static/media/europe.3a299262.png"
+  },
+  {
+    "revision": "4923de2703f55b4751949824681b8955",
+    "url": "/static/media/superfluid_logo.4923de27.svg"
+  }
+]);
